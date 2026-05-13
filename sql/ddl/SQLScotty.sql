dca0c0dd98b012a88174820bab2b5efbc2a1b900
ManuelaPlanelles <manuelaplanelles@gmail.com>
2026-05-13 18:52:56 +0200

    Update SQLScotty.sql
    se actualizan los datos,

--- sql/ddl/SQLScotty.sql
+++ sql/ddl/SQLScotty.sql
@@ -8,18 +8,18 @@ idUser int IDENTITY PRIMARY KEY
 , password varchar(100) NOT NULL
 , role varchar(13) NOT NULL DEFAULT 'ALUMNI'
 , address varchar(200)
-, phone varchar(15)
+, phone char(9)
 , CHECK (email LIKE '_%@_%._%')
 , CHECK (role IN ('ALUMNI', 'ADMINISTRATOR'))
 , CHECK (phone IS NULL OR phone LIKE ('[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'))
 )
 
-CREATE TABLE PRODUCT (
-idProduct int IDENTITY PRIMARY KEY
-, name varchar(100) NOT NULL
-, description varchar(500) NULL
-, price money NOT NULL
-, stock int NOT NULL DEFAULT 0
+CREATE TABLE [PRODUCT] (
+idProduct tinyint IDENTITY PRIMARY KEY
+, name varchar(50) NOT NULL
+, description varchar(200) NULL
+, price smallmoney NOT NULL
+, stock smallint NOT NULL DEFAULT 0
 , category varchar(20) NOT NULL
 , CHECK (price > 0)
 , CHECK (stock >= 0)
@@ -29,7 +29,7 @@ idProduct int IDENTITY PRIMARY KEY
 CREATE TABLE [ORDER] (
 idOrder int IDENTITY PRIMARY KEY
 , date date NOT NULL DEFAULT GETDATE()
-, total money NOT NULL DEFAULT 0
+, total smallmoney NOT NULL DEFAULT 0
 , status varchar(9) NOT NULL DEFAULT 'PENDING'
 , idUser int NOT NULL
 , CHECK (status IN ('PENDING', 'PROCESSED', 'CANCELLED'))
@@ -39,12 +39,12 @@ idOrder int IDENTITY PRIMARY KEY
 
 CREATE TABLE ORDER_DETAILS (
 idDetail int IDENTITY PRIMARY KEY
-, quantity int NOT NULL
-, subtotal money NOT NULL
+, quantity tinyint NOT NULL
+, subtotal smallmoney NOT NULL
 , idOrder int NOT NULL
-, idProduct int NOT NULL
+, idProduct tinyint NOT NULL
 , CHECK (quantity > 0)
 , CHECK (subtotal > 0)
 , FOREIGN KEY (idOrder) REFERENCES [ORDER] (idOrder)
-, FOREIGN KEY (idProduct) REFERENCES PRODUCT(idProduct)
+, FOREIGN KEY (idProduct) REFERENCES [PRODUCT] (idProduct)
 )
